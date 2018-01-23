6a6ded905c6538733706ea493a578e781111adef
Noela Dizdarevic <noelaokanovic@gmail.com>
2018-01-23 15:41:27 +0100

    Initial commit

---
+++ app/src/main/java/com/noela/newsreaderapp/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.noela.newsreaderapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        Intent intent = getIntent();
+        webView.loadData(intent.getStringExtra("content"), "text/html","UTF-8");
+    }
+}
